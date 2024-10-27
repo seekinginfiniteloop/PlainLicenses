0429b232193c709497b5304ea197f4ba1e7ca87f
Adam Poulemanos <adam@plainlicense.org>
2024-10-27 14:18:10 -0400

    added node_modules to tracking for build consistency

--- .devcontainer/container-setup.sh
+++ .devcontainer/container-setup.sh
@@ -8,7 +8,6 @@ sudo apt -y install libcairo2-dev pkg-config python3-dev ripgrep nano git git-ma
 bun install --no-summary --silent
 uv python install cpython-3.13.0-linux-x86_64-gnu -q
 uv tool install ipython -q
-uv tool install ruff -q
 
 export BUNOPTS="--no-summary --silent"
 # Install global npm packages
@@ -18,28 +17,26 @@ bun install -g "${BUNOPTS}" 'eslint'
 bun install -g "${BUNOPTS}" 'prettier'
 bun install -g "${BUNOPTS}" 'semantic-release-cli'
 
+export PYVENV="/workspaces/PlainLicense/.venv/bin/activate"
+
 # update zshrc
 {
-    echo "export PATH='~/.local/share/uv/python/cpython-3.13.0-linux-x86_64-gnu/bin:$PATH'"
-    echo "export PATH='/home/vscode/.local/bin:$PATH'"
-    echo 'export UV_LINK_MODE=copy'
-    echo 'export UV_COMPILE_BYTECODE=1'
-    echo 'export UV_PYTHON_PREFERENCE=managed'
-    echo 'export UV_PYTHON=cpython-3.13.0-linux-x86_64-gnu'
-    echo "alias rg=\"rg --stats --trim --color=always --colors 'match:fg:white' --colors 'path:fg:blue' --smart-case --search-zip\""
+    echo "alias rg=\"rg --no-ignore-vcs --stats --trim --color=always --colors 'match:fg:white' --colors 'path:fg:blue' --smart-case --search-zip\""
     echo "alias rgf='rg --files'"
     echo "alias rgp='rg --pretty'"
     echo "alias rgc='rg --count'"
     echo "autoload -Uz compinit"
     echo "zstyle ':completion:*' menu select"
-    echo "export FORCE_COLOR=1"
-    echo "export CLICOLOR_FORCE=1"
     echo "[ -s \"~/.bun/_bun\" ] && source \"~/.bun/_bun\""
-    echo "export BUN_INSTALL=\"$HOME/.bun\""
-    echo "export PATH=\"$BUN_INSTALL/bin:$PATH\""
     echo 'alias ll="ls -alF"'
-    
+    echo "source \"${PYVENV}\"" >> ~/.zshrc
+    echo "source \"${PYVENV}\"" >> ~/.bashrc
 } >> ~/.zshrc
+
+source "${PYVENV}"
+
+uv sync --all-extras
+uv venv --allow-existing .
 
 # Update git submodules
 git submodule update
@@ -47,13 +44,3 @@ git submodule update
 # Source .zshrc to apply changes
 /bin/zsh -c "source ~/.zshrc"
 /bin/zsh
-uv venv --allow-existing --relocatable .
-
-
-export PYVENV="/workspaces/PlainLicense/.venv/bin/activate"
-echo "export PYVENV=\"${PYVENV}\"" >> ~/.zshrc
-echo "source \"${PYVENV}\"" >> ~/.zshrc
-echo "source \"${PYVENV}\"" >> ~/.bashrc
-source "${PYVENV}"
-
-uv sync --all-extras
