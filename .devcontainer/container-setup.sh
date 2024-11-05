85793afec3b881ebb59f25c98cdf529826b2218d
Adam Poulemanos <adam@plainlicense.org>
2024-11-05 02:56:28 +0000

    small container changes

--- .devcontainer/container-setup.sh
+++ .devcontainer/container-setup.sh
@@ -48,30 +48,34 @@ export UV_PYTHON_DOWNLOADS="automatic"
 curl -LsSf https://astral.sh/uv/install.sh | sh &&
 curl -fsSL https://bun.sh/install | bash &&
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &&
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 source "$HOME/.cargo/env" &&
 export PATH="$HOME/bin:$HOME/sbin:$HOME/.local/sbin:$HOME/.cargo/bin:$PATH:/opt/bin:/opt/sbin:/opt/local/bin:opt/local/sbin"
-echo "This is the path: $PATH"
-
 # Define the block of rc scripting as a variable
 CONFIG_BLOCK=$(cat << 'EOF'
 export BUN_INSTALL="/home/vscode/.bun"
-alias rg='rg --no-ignore-vcs --stats --trim --color=always --colors "match:fg:white" --colors "path:fg:blue" --smart-case --search-zip'
-alias rgf='rg --files'
-alias rgp='rg --pretty'
-alias rgc='rg --count'
-alias ll='ls -alF'
+alias rgg='rg --no-ignore --trim --pretty --colors "match:fg:white" --colors "path:fg:blue" --smart-case --search-zip --hidden'
+alias rgf='rg --files | rgg'
+alias rgc='rgg --count'
+alias ll='ls -alFh'
+alias llr='ls -alFhR'
+alias lld='ls -alFhd'
+alias lldr='ls -alFhdR'
 export PATH="$BUN_INSTALL/bin:$HOME/.cargo/bin:$HOME/bin:$HOME/sbin:$PATH:/opt/bin:/opt/sbin:/opt/local/bin:/opt/local/sbin"
 export UV_PYTHON_DOWNLOADS="automatic"
 export UV_COLOR="always"
+export FORCE_COLOR=1
+export CLICOLOR_FORCE=1
 export FILEHANDLER_ENABLED="true"
 export LOG_PATH="$HOME/logs"
-source "/workspaces/PlainLicense/.venv/bin/activate"
 EOF
 )
 ZCONFIG_BLOCK=$(cat << 'EOF'
 fpath+=~/.zfunc
+setopt extended_glob
 autoload -Uz compinit
 zstyle ':completion:*' menu select
+plugins=(bun git gitfast git-prompt gpg-agent pre-commit zsh-interactive-cd rust)
 EOF
 )
 # Write the block of rc scripting to .zshrc and .bashrc
@@ -80,16 +84,7 @@ echo "$CONFIG_BLOCK" >> "$HOME/.bashrc"
 echo "$ZCONFIG_BLOCK" >> "$HOME/.zshrc"
 # shellcheck disable=SC1090
 source "$HOME/.bashrc"
-bash_completion="$HOME/.local/share/bash-completion/completions"
-mkdir -p "$bash_completion"
-mkdir -p "$HOME/.zfunc"
-"$HOME/.rustup/bin/rustup completions" zsh > "$HOME/.zfunc/_rustup"
-"$HOME/.rustup/bin/rustup completions" bash > "$bash_completion/rustup"
-"$HOME/.cargo/bin/rustup completions" cargo zsh > "$HOME/.zfunc/_cargo"
-"$HOME/.cargo/bin/rustup completions" cargo bash > "$bash_completion/cargo"
-"$HOME/.cargo/bin/rg" --generate zsh > "$HOME/.zfunc/_rg"
-"$HOME/.cargo/bin/rg" --generate bash > "$bash_completion/rg"
-/usr/bin/rg --generate bash >> "$bash_completion/_rg"
+
 
 function uv_install() {
     export UV_PYTHON_DOWNLOADS="automatic"
@@ -111,10 +106,24 @@ function bun_install() {
     $bunloc install -g "${BUNOPTS}" @linthtml/linthtml stylelint eslint prettier semantic-release-cli markdownlint-cli2 commitizen commitlint node
 }
 
+function set_completions() {
+    bash_completion="$HOME/.local/share/bash-completion/completions"
+    mkdir -p "$bash_completion"
+    mkdir -p "$HOME/.zfunc"
+    $HOME/.cargo/bin/rustup completions zsh > "$HOME/.zfunc/_rustup"
+    $HOME/.cargo/bin/rustup completions bash > "$bash_completion/rustup"
+    $HOME/.cargo/bin/rustup completions zsh cargo > "$HOME/.zfunc/_cargo"
+    $HOME/.cargo/bin/rustup completions bash cargo > "$bash_completion/cargo"
+    $HOME/.cargo/bin/rg --generate=complete-zsh > "$HOME/.zfunc/_rg"
+    $HOME/.cargo/bin/rg --generate=complete-bash > "$bash_completion/rg"
+}
+
 export BUNOPTS="--no-interactive --silent"
 
 # Execute functions
+cargo install --all-features ripgrep &&
 cargo install typos-cli &&
+set_completions &&
 uv_install &&
 bun_install &&
 sudo chsh -s /bin/zsh vscode
